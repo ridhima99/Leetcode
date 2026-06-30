2189be161b63f924694aed5638687006c7a09287
ridhima99 <164544971+ridhima99@users.noreply.github.com>
2026-06-30 16:03:10 +0530

    Time: 3 ms (37.87%), Space: 14.2 MB (37.8%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+
+    void solve(int index, vector<int>& candidates, int target) {
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = index; i < candidates.size(); i++) {
+            if (i > index && candidates[i] == candidates[i - 1])
+                continue;
+            if (candidates[i] > target)
+                break;
+            temp.push_back(candidates[i]);
+            solve(i + 1, candidates, target - candidates[i]);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        solve(0, candidates, target);
+        return ans;
+    }
+};
